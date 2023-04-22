04a8e5e608b099b48254295a3f5687f237796a06
Hassel2 <hasselprassel@mail.ru>
2023-04-22 15:49:48 +0300

    feat(database_start.sh): add docker usage

--- database_start.sh
+++ database_start.sh
@@ -9,13 +9,19 @@ print_usage() {
   printf \
 "\nThis script helps you to create or/and run docker mysql container. \n\
 Usage:\n\
-  -s - run existing container\n\
-      example: ./database_start.sh -s -n example\n\
-  -r - create new container\n\
-      example: ./database_start.sh -r -n example -p 1234\n\
-  \n\
-  -n - name of the containter\n\
-  -p - mysql root password\
+    -s - run existing container\n\
+        example: ./database_start.sh -s -n example\n\
+    -r - create new container\n\
+        example: ./database_start.sh -r -n example -p 1234\n\
+\n\
+    -n - name of the containter\n\
+    -p - mysql root password\n\
+\n\
+Docker example:
+    \$ docker ps - to list all running containers
+    \$ docker ps -a - to list all existing containers
+    \$ docker kill [name] - to stop container
+    \$ docker logs [name] - to see container logs
 "
 }
 
@@ -36,7 +42,7 @@ if $s_flag; then
         exit 1;
     fi;
 
-    docker start -i $name;
+    docker start -i $name; &
     exit 0;
 fi;
 
@@ -46,6 +52,6 @@ if $r_flag; then
         exit 1;
     fi;
 
-    docker run --name $name -e MYSQL_ROOT_PASSWORD=$password -e LANG=C.UTF-8 -d mysql/mysql-server:5.7
+    docker run --name $name -e MYSQL_ROOT_PASSWORD=$password -e LANG=C.UTF-8 -d mysql/mysql-server:5.7 &
     exit 0;
 fi;
