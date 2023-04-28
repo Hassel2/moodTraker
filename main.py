eb709cbd2be4964904f6c536e66c4228aaedeffd
Hassel2 <hasselprassel@mail.ru>
2023-04-28 23:14:34 +0300

    feat(main.py): add command line arguments

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+import argparse
 from database.database import Database
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--setup', action='store_true', help='create and migrate database schema')
+    parser.add_argument('-r', '--run', action='store_true', help='run application')
+    args = parser.parse_args()
+
+    if args.setup and args.run:
+        exit(1)
+
+    if args.setup:
+        setup()
+        exit(0)
+
+    if args.run:
+        start()
+        exit(0)
+
+
+def start():
+    pass
+
+
+def setup():
     db = Database()
     db.migrate()
+
 
 if __name__ == "__main__":
     main()
