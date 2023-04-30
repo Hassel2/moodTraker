74289fa9b39ad301a10991ba9f8ae24149a2b2c2
Hassel2 <hasselprassel@mail.ru>
2023-04-30 21:03:52 +0300

    style(app.py): change form buttons layout

--- app.py
+++ app.py
@@ -58,26 +58,18 @@ class App:
     ) -> None:
         keyboard = [
             [
-                InlineKeyboardButton("7", callback_data="1"),
-                InlineKeyboardButton("8", callback_data="2"),
-                InlineKeyboardButton("9", callback_data="3"),
-            ], [
-                InlineKeyboardButton("4", callback_data="3"),
-                InlineKeyboardButton("5", callback_data="3"),
-                InlineKeyboardButton("6", callback_data="3"),
-            ], [
-                InlineKeyboardButton("1", callback_data="3"),
-                InlineKeyboardButton("2", callback_data="3"),
-                InlineKeyboardButton("3", callback_data="3"),
-            ], [
-                InlineKeyboardButton("10", callback_data="3")
-            ],
+                InlineKeyboardButton("😢", callback_data="1"),
+                InlineKeyboardButton("🙁", callback_data="2"),
+                InlineKeyboardButton("😐", callback_data="3"),
+                InlineKeyboardButton("🙂", callback_data="4"),
+                InlineKeyboardButton("😀", callback_data="5"),
+            ]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text(
-                "Оцените свое эмоциональное состояние по десятибальной шкале:", 
+                "Оцените свое эмоциональное состояние по пятибальной шкале:", 
                 reply_markup=reply_markup
         )
 
@@ -89,9 +81,19 @@ class App:
     ) -> None:
         """Parses the CallbackQuery and updates the message text."""
         query = update.callback_query
+        id_chat = update.effective_chat.id
+
+        Database.form_answer(id_chat, query.data)
 
         # CallbackQueries need to be answered, even if no notification to the user is needed
         # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-        await query.answer()
+        await query.answer(
+            text=["😢", "🙁", "😐", "🙂", "😀"][int(query.data) - 1]
+        )
 
-        await query.edit_message_text(text=f"Selected option: {query.data}")
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=["😢", "🙁", "😐", "🙂", "😀"][int(query.data) - 1]
+        )
+
+        await query.edit_message_text(text="Спасибо, ваш ответ записан!")
